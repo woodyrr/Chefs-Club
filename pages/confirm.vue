6796a9f7e8b084b4199a4e239bf2deb890824f4c
Woody Ramey <woodyramey@icloud.com>
2024-09-12 23:15:36 -0700

    create skeleton for confirm page

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const user = useSupabaseUser()
-
+import { Skeleton } from '@/components/ui/skeleton'
 watch(user, () => {
   if (user.value) {
     return navigateTo('/home')
@@ -9,9 +9,42 @@ watch(user, () => {
 </script>
 
 <template>
-  <div>
-    <p>
-      Redirecting...
-    </p>
+    <div class="w-full flex flex-col gap-8">
+        <Skeleton class="h-10 w-[90%]" />
+        <div class="flex justify-around">
+            <Skeleton class="h-[90%] w-[25%]" />
+            <div class="flex flex-col gap-5">
+                <div class="flex items-center space-x-4">
+                    <Skeleton class="h-12 w-12 rounded-full" />
+                    <div class="space-y-2">
+                    <Skeleton class="h-4 w-[250px]" />
+                    <Skeleton class="h-4 w-[200px]" />
+                    </div>
+                </div>
+                <div class="flex items-center space-x-4">
+                    <Skeleton class="h-12 w-12 rounded-full" />
+                    <div class="space-y-2">
+                    <Skeleton class="h-4 w-[250px]" />
+                    <Skeleton class="h-4 w-[200px]" />
+                    </div>
+                </div>
+                <div class="flex items-center space-x-4">
+                    <Skeleton class="h-12 w-12 rounded-full" />
+                    <div class="space-y-2">
+                    <Skeleton class="h-4 w-[250px]" />
+                    <Skeleton class="h-4 w-[200px]" />
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+    </div>
+  <div class="flex items-center space-x-4">
+    <Skeleton class="h-12 w-12 rounded-full" />
+    <div class="space-y-2">
+      <Skeleton class="h-4 w-[250px]" />
+      <Skeleton class="h-4 w-[200px]" />
+    </div>
   </div>
 </template>
